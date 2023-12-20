e69faf59cb4680de872484a4565bc5d6155a1c2c
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-12-20 09:54:33 -0300

    events firing: example

--- events_firing.py
+++ events_firing.py
@@ -1,22 +1,23 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import Edge
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 from selenium.webdriver.common.by import By
 
 class MyListener(AbstractEventListener):
     def after_click(self, element, driver) -> None:
         if element.tag_name == 'input':
-            print(dir(element))
-            print('Depois do clique no Input')
-            print(driver.find_element(By.TAG_NAME, 'label').text)
+            attr = element.get_attribute("id")
+            print(driver.find_element(By.ID, f'l{attr}').text)
 
 
-browser = Firefox()
+browser = Edge()
 browser.implicitly_wait(10)
 
 wrapper = EventFiringWebDriver(browser, MyListener())
 wrapper.get("https://selenium.dunossauro.live/exercicio_07")
 
-input_el = wrapper.find_element(By.TAG_NAME, 'input').click()
+input_el = wrapper.find_element(By.ID, 'nome').click()
+input_el = wrapper.find_element(By.ID, 'email').click()
+input_el = wrapper.find_element(By.ID, 'senha').click()
 
 
-browser.quit()
+# browser.quit()
